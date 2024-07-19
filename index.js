a48cb7f695d89a92d0f409b6bdb54dd72f625fa9
Viggen <koriremanuel0@gmail.com>
2024-07-19 10:02:22 +0300

    Event Lestaner Update

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 function addingEventListener() {
+    button.addEventListener('click', function() {
+        console.log('clicked');
+    })
 }
+
+console.log(addingEventListener())
+
+const input = document.getElementById('button');
+
+function clickAlert() {
+  alert('I was clicked!');
+}
+
+input.addEventListener('click', clickAlert);
